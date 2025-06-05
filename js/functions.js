66b747d4a6b43b6976c2e45776f51bb15bf2531c
dreem <d.larionoff@yandex.ru>
2025-06-05 12:13:48 +0300

    Добавляет функции

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,26 @@
-function checkStringLength (string, maxlength) {
-  return string.length <= maxlength;
+function checkStringLength (string, length) {
+  return string.length <= length;
 }
 
 
 function checkPolindrom (string) {
+  string = string.toLowerCase();
+  string = string.replaceAll(' ', '');
   let emptyString = '';
-  for (let i = string.length - 1; i >= 0; --i) {
+  for (let i = string.length - 1; i >= 0; i--) {
     emptyString += string[i];
   }
-  return string == emptyString;
+  return string === emptyString;
 }
 
+function getNumbers (string) {
+  string = string.toString();
+  let numbers = '';
+  for (let i = 0; i <= string.length; i++) {
+    if (Number.isNaN(parseInt(string[i], 10)) === false) {
+      numbers += string[i];
+    }
+  }
+  return numbers;
+}
 
