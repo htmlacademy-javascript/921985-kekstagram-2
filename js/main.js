bacf592da2a82c54510ad62c4b08f87ae6792d93
dreem <d.larionoff@yandex.ru>
2025-06-18 12:24:03 +0300

    Добавляет функцию создания массива с описанием фотографий

---
+++ js/main.js
@@ -0,0 +1,50 @@
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const NAMES = [
+  'Олег',
+  'Егор',
+  'Ирина',
+  'Мария',
+  'Андрей',
+  'Василий',
+  'Полина',
+  'Сабрина'
+];
+
+const getRandomNumber = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomElementArray = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const createComment = () => {
+  return {
+    id: getRandomNumber(0, 999),
+    avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+    message: `${getRandomElementArray(MESSAGES)}`,
+    name: `${getRandomElementArray(NAMES)}`
+  };
+};
+
+const createObject = () => {
+  return {
+    id: getRandomNumber(1, 25),
+    url: `photos/${getRandomNumber(1, 25)}.jpg`,
+    description: `Описание фотографии №${getRandomNumber(1, 25)}`,
+    likes: getRandomNumber(15, 200),
+    comments: Array.from({length: getRandomNumber(0, 30)}, createComment)
+  };
+};
+
+const objectArray = Array.from({length: 25}, createObject);
+
+console.log(objectArray);
